2c8b25949d1e1e1dda18ebcb1625b20cfecbcc44
pondhanush764 <77870791+pondhanush764@users.noreply.github.com>
2021-03-17 10:57:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Bot(
     ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
+    ,2163126
+    ,81601a0c7b0b31aaf077fb1b3860d02d
+    bot_token=1768857439:AAGue_HupgnUehLBrZGPZ88SydGIfT2berI
     plugins=dict(root="handlers")
 )
 
